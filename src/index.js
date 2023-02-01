3acc231e1de9b02054588906ee170d8a2cc6b636
1Leep <anamariafakezaum@gmail.com>
2023-02-01 19:52:34 -0300

    fix: changes useChrome to false on client instance

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ require('../extended/to-capitalize')
 create({
     sessionId: "BOT_RPG",
     multiDevice: true,
-    useChrome: true,
+    useChrome: false,
     authTimeout: 0,
     blockCrashLogs: true,
     disableSpins: true,
